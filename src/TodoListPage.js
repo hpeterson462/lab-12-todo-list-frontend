d38851866327e43aeaa9a46622d12c79c6c4cc19
Heather Peterson <hpeterson462@gmail.com>
2020-08-19 18:45:43 -0700

    todos render to page

--- src/TodoListPage.js
+++ src/TodoListPage.js
@@ -1,18 +1,20 @@
 import React, { Component } from 'react'
 import { fetchTodos, createTodo } from './todos-api.js';
+import { Link } from 'react-router-dom';
 
 export default class TodoListPage extends Component {
     state = {
         name: '',
         completed: false,
-        owner_id: 1
+        owner_id: 1,
+        todos: []
     }
 
     componentDidMount = async () => {
         if (!this.props.token) {
             this.props.history.push('/signin');
         } else {
-            const todosData = await fetchTodos();
+            const todosData = await fetchTodos(this.props.token);
 
             this.setState({
                 todos: todosData.body
@@ -46,15 +48,26 @@ export default class TodoListPage extends Component {
 
     render() {
         return (
-            <div className='content'>
-                <h2>Todo:</h2>
-                <form onSubmit={this.handleSubmit}>
-                    <label>
-                        <input onChange={this.handleTodoChange} value={this.state.name} />
-                    </label>
-                    <button>Create Todo</button>
-                </form>
-            </div>
+            <>
+                <div className='create-todos'>
+                    <h2>Todo:</h2>
+                    <form onSubmit={this.handleSubmit}>
+                        <label>
+                            <input onChange={this.handleTodoChange} value={this.state.name} />
+                        </label>
+                        <button>Create Todo</button>
+                    </form>
+                </div>
+                <div className='todos'>
+                    {
+                        this.state.todos.map((todo) => {
+                            return <Link className='todo' key={todo.name}>
+                                <p>{todo.name}</p>
+                            </Link>
+                        })
+                    }
+                </div>
+            </>
         )
     }
 }
